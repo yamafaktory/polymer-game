005636e342a265af3a4994f25fe8d0c0acadf140
yamafaktory <yamafaktory@gmail.com>
2014-05-30 13:57:17 +0200

    switch to core-animation

--- bower_components/PathFinding.js/lib/pathfinding-browser.js
+++ bower_components/PathFinding.js/lib/pathfinding-browser.js
@@ -398,10 +398,11 @@ module.exports = {
     'BiBreadthFirstFinder' : require('./finders/BiBreadthFirstFinder'),
     'BiDijkstraFinder'     : require('./finders/BiDijkstraFinder'),
     'JumpPointFinder'      : require('./finders/JumpPointFinder'),
-    'IDAStarFinder'        : require('./finders/IDAStarFinder')
+    'IDAStarFinder'        : require('./finders/IDAStarFinder'),
+    'OrthogonalJumpPointFinder' : require('./finders/OrthogonalJumpPointFinder')
 };
 
-},{"./core/Grid":4,"./core/Heuristic":5,"./core/Node":6,"./core/Util":7,"./finders/AStarFinder":8,"./finders/BestFirstFinder":9,"./finders/BiAStarFinder":10,"./finders/BiBestFirstFinder":11,"./finders/BiBreadthFirstFinder":12,"./finders/BiDijkstraFinder":13,"./finders/BreadthFirstFinder":14,"./finders/DijkstraFinder":15,"./finders/IDAStarFinder":16,"./finders/JumpPointFinder":17,"heap":1}],4:[function(require,module,exports){
+},{"./core/Grid":4,"./core/Heuristic":5,"./core/Node":6,"./core/Util":7,"./finders/AStarFinder":8,"./finders/BestFirstFinder":9,"./finders/BiAStarFinder":10,"./finders/BiBestFirstFinder":11,"./finders/BiBreadthFirstFinder":12,"./finders/BiDijkstraFinder":13,"./finders/BreadthFirstFinder":14,"./finders/DijkstraFinder":15,"./finders/IDAStarFinder":16,"./finders/JumpPointFinder":17,"./finders/OrthogonalJumpPointFinder":18,"heap":1}],4:[function(require,module,exports){
 var Node = require('./Node');
 
 /**
@@ -1789,16 +1790,16 @@ JumpPointFinder.prototype._identifySuccessors = function(node) {
  */
 JumpPointFinder.prototype._jump = function(x, y, px, py) {
     var grid = this.grid,
-        dx = x - px, dy = y - py, jx, jy;
+        dx = x - px, dy = y - py;
 
     if (!grid.isWalkableAt(x, y)) {
         return null;
     }
-    
+
     if(this.trackJumpRecursion === true) {
         grid.getNodeAt(x, y).tested = true;
     }
-    
+
     if (grid.getNodeAt(x, y) === this.endNode) {
         return [x, y];
     }
@@ -1829,9 +1830,7 @@ JumpPointFinder.prototype._jump = function(x, y, px, py) {
 
     // when moving diagonally, must check for vertical/horizontal jump points
     if (dx !== 0 && dy !== 0) {
-        jx = this._jump(x + dx, y, x, y);
-        jy = this._jump(x, y + dy, x, y);
-        if (jx || jy) {
+        if (this._jump(x + dx, y, x, y) || this._jump(x, y + dy, x, y)) {
             return [x, y];
         }
     }
@@ -1888,9 +1887,7 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
         else {
             if(dx === 0) {
                 if (grid.isWalkableAt(x, y + dy)) {
-                    if (grid.isWalkableAt(x, y + dy)) {
-                        neighbors.push([x, y + dy]);
-                    }
+                    neighbors.push([x, y + dy]);
                     if (!grid.isWalkableAt(x + 1, y)) {
                         neighbors.push([x + 1, y + dy]);
                     }
@@ -1901,9 +1898,7 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
             }
             else {
                 if (grid.isWalkableAt(x + dx, y)) {
-                    if (grid.isWalkableAt(x + dx, y)) {
-                        neighbors.push([x + dx, y]);
-                    }
+                    neighbors.push([x + dx, y]);
                     if (!grid.isWalkableAt(x, y + 1)) {
                         neighbors.push([x + dx, y + 1]);
                     }
@@ -1922,12 +1917,139 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
             neighbors.push([neighborNode.x, neighborNode.y]);
         }
     }
-    
+
     return neighbors;
 };
 
 module.exports = JumpPointFinder;
 
-},{"../core/Heuristic":5,"../core/Util":7,"heap":1}]},{},[3])(3)
+},{"../core/Heuristic":5,"../core/Util":7,"heap":1}],18:[function(require,module,exports){
+/**
+ * @author imor / https://github.com/imor
+ */
+var Heuristic  = require('../core/Heuristic');
+var JumpPointFinder = require('./JumpPointFinder');
+
+/**
+ * Path finder using the Jump Point Search algorithm allowing only horizontal
+ * or vertical movements.
+ * @param {object} opt
+ * @param {function} opt.heuristic Heuristic function to estimate the distance
+ *     (defaults to manhattan).
+ */
+function OrthogonalJumpPointFinder(opt) {
+    JumpPointFinder.call(this, opt);
+    opt = opt || {};
+    this.heuristic = opt.heuristic || Heuristic.manhattan;
+}
+
+OrthogonalJumpPointFinder.prototype = new JumpPointFinder();
+OrthogonalJumpPointFinder.prototype.constructor = OrthogonalJumpPointFinder;
+
+/**
+ * Search recursively in the direction (parent -> child), stopping only when a
+ * jump point is found.
+ * @protected
+ * @return {Array.<[number, number]>} The x, y coordinate of the jump point
+ *     found, or null if not found
+ */
+OrthogonalJumpPointFinder.prototype._jump = function(x, y, px, py) {
+    var grid = this.grid,
+        dx = x - px, dy = y - py;
+
+    if (!grid.isWalkableAt(x, y)) {
+        return null;
+    }
+
+    if(this.trackJumpRecursion === true) {
+        grid.getNodeAt(x, y).tested = true;
+    }
+
+    if (grid.getNodeAt(x, y) === this.endNode) {
+        return [x, y];
+    }
+
+    if (dx !== 0) {
+        if ((grid.isWalkableAt(x, y - 1) && !grid.isWalkableAt(x - dx, y - 1)) ||
+            (grid.isWalkableAt(x, y + 1) && !grid.isWalkableAt(x - dx, y + 1))) {
+            return [x, y];
+        }
+    }
+    else if (dy !== 0) {
+        if ((grid.isWalkableAt(x - 1, y) && !grid.isWalkableAt(x - 1, y - dy)) ||
+            (grid.isWalkableAt(x + 1, y) && !grid.isWalkableAt(x + 1, y - dy))) {
+            return [x, y];
+        }
+        //When moving vertically, must check for horizontal jump points
+        if (this._jump(x + 1, y, x, y) || this._jump(x - 1, y, x, y)) {
+            return [x, y];
+        }
+    }
+    else {
+        throw new Error("Only horizontal and vertical movements are allowed");
+    }
+
+    return this._jump(x + dx, y + dy, x, y);
+};
+
+/**
+ * Find the neighbors for the given node. If the node has a parent,
+ * prune the neighbors based on the jump point search algorithm, otherwise
+ * return all available neighbors.
+ * @return {Array.<[number, number]>} The neighbors found.
+ */
+OrthogonalJumpPointFinder.prototype._findNeighbors = function(node) {
+    var parent = node.parent,
+        x = node.x, y = node.y,
+        grid = this.grid,
+        px, py, nx, ny, dx, dy,
+        neighbors = [], neighborNodes, neighborNode, i, l;
+
+    // directed pruning: can ignore most neighbors, unless forced.
+    if (parent) {
+        px = parent.x;
+        py = parent.y;
+        // get the normalized direction of travel
+        dx = (x - px) / Math.max(Math.abs(x - px), 1);
+        dy = (y - py) / Math.max(Math.abs(y - py), 1);
+
+        if (dx !== 0) {
+            if (grid.isWalkableAt(x, y - 1)) {
+                neighbors.push([x, y - 1]);
+            }
+            if (grid.isWalkableAt(x, y + 1)) {
+                neighbors.push([x, y + 1]);
+            }
+            if (grid.isWalkableAt(x + dx, y)) {
+                neighbors.push([x + dx, y]);
+            }
+        }
+        else if (dy !== 0) {
+            if (grid.isWalkableAt(x - 1, y)) {
+                neighbors.push([x - 1, y]);
+            }
+            if (grid.isWalkableAt(x + 1, y)) {
+                neighbors.push([x + 1, y]);
+            }
+            if (grid.isWalkableAt(x, y + dy)) {
+                neighbors.push([x, y + dy]);
+            }
+        }
+    }
+    // return all neighbors
+    else {
+        neighborNodes = grid.getNeighbors(node, false);
+        for (i = 0, l = neighborNodes.length; i < l; ++i) {
+            neighborNode = neighborNodes[i];
+            neighbors.push([neighborNode.x, neighborNode.y]);
+        }
+    }
+
+    return neighbors;
+};
+
+module.exports = OrthogonalJumpPointFinder;
+
+},{"../core/Heuristic":5,"./JumpPointFinder":17}]},{},[3])(3)
 });
 ;
--- bower_components/PathFinding.js/src/finders/JumpPointFinder.js
+++ bower_components/PathFinding.js/src/finders/JumpPointFinder.js
@@ -119,16 +119,16 @@ JumpPointFinder.prototype._identifySuccessors = function(node) {
  */
 JumpPointFinder.prototype._jump = function(x, y, px, py) {
     var grid = this.grid,
-        dx = x - px, dy = y - py, jx, jy;
+        dx = x - px, dy = y - py;
 
     if (!grid.isWalkableAt(x, y)) {
         return null;
     }
-    
+
     if(this.trackJumpRecursion === true) {
         grid.getNodeAt(x, y).tested = true;
     }
-    
+
     if (grid.getNodeAt(x, y) === this.endNode) {
         return [x, y];
     }
@@ -159,9 +159,7 @@ JumpPointFinder.prototype._jump = function(x, y, px, py) {
 
     // when moving diagonally, must check for vertical/horizontal jump points
     if (dx !== 0 && dy !== 0) {
-        jx = this._jump(x + dx, y, x, y);
-        jy = this._jump(x, y + dy, x, y);
-        if (jx || jy) {
+        if (this._jump(x + dx, y, x, y) || this._jump(x, y + dy, x, y)) {
             return [x, y];
         }
     }
@@ -218,9 +216,7 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
         else {
             if(dx === 0) {
                 if (grid.isWalkableAt(x, y + dy)) {
-                    if (grid.isWalkableAt(x, y + dy)) {
-                        neighbors.push([x, y + dy]);
-                    }
+                    neighbors.push([x, y + dy]);
                     if (!grid.isWalkableAt(x + 1, y)) {
                         neighbors.push([x + 1, y + dy]);
                     }
@@ -231,9 +227,7 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
             }
             else {
                 if (grid.isWalkableAt(x + dx, y)) {
-                    if (grid.isWalkableAt(x + dx, y)) {
-                        neighbors.push([x + dx, y]);
-                    }
+                    neighbors.push([x + dx, y]);
                     if (!grid.isWalkableAt(x, y + 1)) {
                         neighbors.push([x + dx, y + 1]);
                     }
@@ -252,7 +246,7 @@ JumpPointFinder.prototype._findNeighbors = function(node) {
             neighbors.push([neighborNode.x, neighborNode.y]);
         }
     }
-    
+
     return neighbors;
 };
 
